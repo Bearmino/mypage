d21040133ec92fe34dfc78d4e55fe6399fa5c45a
Minho <116548352+Bearmino@users.noreply.github.com>
2023-03-19 08:03:18 +0900

    Add files via upload

---
+++ hello..py
@@ -0,0 +1,33 @@
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://sparta:test@clustermin.c5qq9dk.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.naver?sel=pnt&date=20210829',headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+# 코딩 시작
+
+trs = soup.select('#old_content > table > tbody > tr')
+for tr in trs:
+    a = tr.select_one('td.title > div > a')
+    if a is not None:
+        title = a.text
+        rank = tr.select_one('td:nth-child(1) > img')['alt']
+        star = tr.select_one('td.point').text
+        doc = {
+            'title':title,
+            'rank':rank,
+            'star':star
+        }
+        db.movies.insert_one(doc)
+          
+#old_content > table > tbody > tr:nth-child(2)
+#old_content > table > tbody > tr:nth-child(3)
+#old_content > table > tbody > tr:nth-child(3) > td.title > div > a
+#old_content > table > tbody > tr:nth-child(2) > td:nth-child(1) > img
+#old_content > table > tbody > tr:nth-child(2) > td.point
