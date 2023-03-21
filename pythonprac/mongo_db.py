ffb2026d485fe3788127a06c9902e16f03bb10ee
Bearmin <minho51y@gmail.com>
2023-03-21 10:25:00 +0900

    pythonprac

--- pythonprac/mongo_db.py
+++ pythonprac/mongo_db.py
@@ -1,18 +1,18 @@
 from pymongo import MongoClient #pip install pymongo dnspython으로 패키지 설치
-client = MongoClient('mongodb+srv://sparta:test@clustermin.c5qq9dk.mongodb.net/?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://testmino:test@clustermino.440aylp.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
 
 #'users'라는 collection에 {'name':,'age':숫자}를 넣습니다.
-#  doc = {
-#     'name':'영수',
-#     'age':24
-#  }
+doc = {
+     'name':'영수',
+     'age':24
+  }
 
-#  db.users.insert_one(doc)
+# db.users.insert_one(doc) #'users' collection에 하나의 데이터만 삽입
 
-#  db.users.insert_one({'name':'영희','age':30})
-#  db.users.insert_one({'name':'민식','age':40})
-#  db.users.insert_one({'name':'철수','age':24})
+db.users.insert_one({'name':'영희','age':30})
+db.users.insert_one({'name':'민식','age':40})
+db.users.insert_one({'name':'철수','age':24})
 
 #모든 데이터 뽑아보기(find)
 all_users = list(db.users.find({},{'_id':False}))
@@ -34,3 +34,5 @@ print(user_one)
 
 #값 삭제하기(delete_one)
 db.users.delete_one({'name':'철수'})
+
+
